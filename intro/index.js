411c8b63956274aee48d90c3c9c19879036c8018
Pankrit Jindal <pankritjindal30@gmail.com>
2022-09-08 18:07:06 +0530

    more on strings/arrays/functions

--- intro/index.js
+++ intro/index.js
@@ -102,3 +102,121 @@ var firstName = "Lovelace";
 firstLetterOfFirstName = firstName[0];
 console.log(firstLetterOfFirstName);
 
+//---------------------------------------------------------------
+// FUNCTIONS 
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    var result = ""; 
+    result += "The "+myAdjective+" "+myNoun+" "+myVerb+" to the store "+myAdverb+".";
+    return result;
+}
+
+console.log(wordBlanks("dog", "big", "ran", "quickly"));
+console.log(wordBlanks("bike","slow","flew","quickly"));
+
+//---------------------------------------------------------------
+// Arrays
+
+// any data type in an array
+var ourArray = ["John", 23];
+var ourArray2 = [["John", 23], ["everything", 101010]];
+
+var myArray = [["Quincy", 1],["string", 23]];
+
+
+// arrays can be changed
+var numArr = [18, 12, 32];
+numArr[1] = 123;
+console.log(numArr);
+
+var myArr = [[1,2,3],[4,5,6],[[12,13,14],12]];
+console.log(myArr[3][0]);
+
+// appending in an array to the end: use push()
+var thatArr = [["string", 12],["that", 13]];
+thatArr.push(["what", 14]);
+console.log(thatArr[2]);
+
+
+// remove items from the array from the end: use pop()
+thatArr = [["John",23],["jordan", 7]];
+var removedElement = thatArr.pop();
+console.log(thatArr);
+console.log(removedElement);
+
+
+// removing the first element instead of the last one: use shift()
+thatArr = [["John",23],["jordan", 7]];
+var removedElement = thatArr.shift();
+console.log(thatArr);
+console.log(removedElement);
+
+// adding elements to the beginning of the array: use unshift()
+thatArr = [["John",23],["jordan", 7]];
+thatArr.shift();
+thatArr.unshift(["Ronaldo", 11]);
+console.log(thatArr);
+
+//---------------------------------------------------------------
+// NESTED ARRAY
+var myList = [["cereal", 3],["milk",2],["banana", 3],["juice", 2], ["eggs", 12]];
+
+//---------------------------------------------------------------
+// FUNCTIONS
+
+function ourReusableFunction() {
+    console.log("Heyya, World");
+}
+
+ourReusableFunction();
+ourReusableFunction();
+ourReusableFunction();
+
+
+//---------------------------------------------------------------
+// SCOPE OF VARIABLES
+
+var myGlobal = 10;
+
+function fun1() {
+    oopsGlobal = 5;     // NOTE:::::::: if no use of "var" keyword in a function, then the variable becomes global automatically
+}
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
+fun1();
+fun2();
+
+
+// LOCAL SCOPE
+var outerWear = "T-Shirt";
+function myOutfit() {
+    var outerWear = "sweater";
+    return outerWear;
+}
+
+console.log(myOutfit());    // returns "sweater"
+console.log(outerWear);     // returns "T-Shirt"
+
+// RETURNING A VALUE FROM A FUNCTION
+function minusSeven(num) {
+    return num - 7;
+}
+console.log(minusSeven(10));
+
+//---------------------------------------------------------------
+// UNDERSTANDING "undefined" VALUE RETURNED FROM A FUNCTION
+
+
+
+
+//---------------------------------------------------------------
+//---------------------------------------------------------------
